80778aa5d8bb7fe8bc9809fa21994f3b59f82823
Elena <elenamalinina2010@yandex.ru>
2021-07-27 23:46:40 +0300

    Initial commit

---
+++ src/geekbrains/lesson_1/HomeWorkApp.java
@@ -0,0 +1,10 @@
+package geekbrains.lesson_1;
+
+public class HomeWorkApp {
+
+    public static void printThreeWords () {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
